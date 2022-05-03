8fd4c9cf1adece3d1445f33e3ffea1c158ef66fc
Feng Zhu <fzhu2e@outlook.com>
2022-05-03 22:01:44 +0800

    Fix bugs in ml.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as fh:
 
 setup(
     name='cfr',  # required
-    version='0.0.5',
+    version='0.0.6',
     description='cfr: the library for climate field reconstruction',
     long_description=long_description,
     long_description_content_type='text/x-rst',

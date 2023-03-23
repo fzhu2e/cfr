b6c52f77673103656f2ccac321ce9980a9e5106c
Feng Zhu <fzhu2e@outlook.com>
2023-03-23 10:39:13 -0600

    Fix bug in ClimateField.load_nc() with other lat/lon names

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as fh:
 
 setup(
     name='cfr',  # required
-    version='0.2.8',
+    version='0.2.9',
     description='cfr: the Python package for Climate Field Reconstruction',
     long_description=long_description,
     long_description_content_type='text/x-rst',

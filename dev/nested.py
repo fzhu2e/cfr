483c6892bfef162940cce24db1ab28fa71967ed0
CommonClimate <julieneg@usc.edu>
2023-07-06 11:16:37 -0700

    Update nested.py

--- dev/nested.py
+++ dev/nested.py
@@ -7,12 +7,19 @@ Created on Wed Jul  5 09:42:56 2023
 """
 
 import cfr
+import matplotlib.pyplot as plt
 cfr.__version__
 pdb = cfr.ProxyDatabase().fetch('PAGES2kv2')
 
-# standardize
-ref_period = [1951,1980]
-pdb_s = pdb.standardize(ref_period)
 
-# find a nest
-ind = pdb_s.nest_indices([1300, 1400])  
+fig, axs = plt.subplots(nrows=5, ncols=2, figsize=[10,2])
+axs = axs.flatten()
+start = 1000
+for k in range(9):
+    # find a nest
+    century = [start+k*100+1, start+(k+1)*100]
+    pid = pdb.nest_indices(time_period=century)  
+    # filter
+    comp = pdb.filter(by='pid', keys=pid).make_composite(bin_width=5,n_bootstraps=10)
+    comp.plot_composite(ax=axs[k])
+    axs[k].set_title(f'{century}') 

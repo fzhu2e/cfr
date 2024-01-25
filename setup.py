dd7e546aea6231bb577ea901e873291187945d41
Feng Zhu <fzhu2e@outlook.com>
2024-01-24 19:33:22 -0700

    Update on ReconJob.calib_psms()

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='cfr',  # required
-    version='2024.1.15',
+    version='2024.1.24',
     description='cfr: a Python package for Climate Field Reconstruction',
     long_description=long_description,
     long_description_content_type='text/x-rst',

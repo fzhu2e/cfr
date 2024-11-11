da4588e70fb2722e5a88b4491c11e5e1d68868f4
CommonClimate <julieneg@usc.edu>
2024-11-11 10:46:37 -0800

    tick package version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='cfr',  # required
-    version='2024.11.6',
+    version='2024.11.11',
     description='cfr: a Python package for Climate Field Reconstruction',
     long_description=long_description,
     long_description_content_type='text/x-rst',

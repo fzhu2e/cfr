e85ef085fac3c6800cbf1dfc4faf985af197b67d
fzhu2e <fzhu2e@outlook.com>
2026-01-31 12:49:11 -0700

    clean

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='cfr',  # required
-    version='2025.7.28',
+    version='2026.1.31',
     description='cfr: a Python package for Climate Field Reconstruction',
     long_description=long_description,
     long_description_content_type='text/x-rst',
